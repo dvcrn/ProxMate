5e3cc54d1f9d2801695ee5786516d44f23ac4e65
David Mohl <david.mohl@ymail.com>
2012-01-24 11:51:31 +0100

    ordnertrennung

---
+++ chrome/grooveshark.js
@@ -0,0 +1,19 @@
+resetProxy();
+
+$(document).ready(function() {
+	// Check for the broken heart
+	var broken = $("#heartbroken");
+	if (broken.length > 0) {
+
+		// Change text
+		$("#content h2").html("Blah blah blah not available. <br /> ProxMate will unblock Grooveshark now!");
+
+		// Change Icon
+		$("#heartbroken").css("background", "url('http://i.imgur.com/hKjFv.gif')");
+
+		proxifyUri(window.location, true);
+	} else {
+		// Fire event to background page
+		// Will remove the proxy
+	}
+});
---
+++ chrome/youtube-search.js
@@ -0,0 +1,19 @@
+resetProxy();	
+
+$(document).ready(function() {
+
+	console.info($("#proxmate-button > span").html());
+	if (getUrlParam('unblocked') != "true")
+	{
+		var button = '<div><button id="proxmate-button" type="button" class="yt-uix-button yt-uix-button-text yt-uix-button-toggle"><span class="yt-uix-button-content">Unblock this search</span></button></div>';
+		// Append it in the option line
+		$(button).insertAfter($("#search-option-expander"));
+		$("#proxmate-button").click(function() {
+			var oldhtml = $("#proxmate-button span").html();
+			$("#proxmate-button span").html(oldhtml + " <img src='"+chrome.extension.getURL("load.gif")+"' />");
+
+			proxifyUri(window.location.href + "&unblocked=true");
+		});	
+	}
+
+});
