fb7698dedcc2c8d4807d3a40957aba3c2026b608
David Mohl <david.mohl@hotmail.de>
2012-01-25 10:57:54 +0100

    firstart und icon click funktionalität

--- chrome/background.js
+++ chrome/background.js
@@ -1,3 +1,51 @@
+var setPluginStatus = function() 
+{
+	var toggle = localStorage["status"];
+
+	console.info("Toggle: " + toggle);
+
+	if (toggle === undefined) 
+	{
+		localStorage["status"] = true;
+	}
+
+	// Wenn Toggle = False ist, das icon farbig machen
+	if (toggle == "true") {
+		chrome.browserAction.setIcon({
+			path: "images/icon48_gray.png"
+		});
+
+		localStorage["status"] = false;
+	}
+	else
+	{
+		chrome.browserAction.setIcon({
+			path: "images/icon48.png"
+		});
+
+		localStorage["status"] = true;
+	}
+}
+
+// chrome mag anonyme funktionen nicht, also funktion definieren und direkt aufrufen
+
+var checkFirstStart = function() {
+	var firstStart = localStorage["firststart"];
+	console.info("FirstStart: " + firstStart);
+
+	if (firstStart === undefined || firstStart == "true") {
+		chrome.tabs.create(
+		{
+			url: "http://www.personalitycores.com/projects/proxmate"
+		});
+
+		localStorage["firststart"] = false;
+	}
+};
+checkFirstStart();
+
+chrome.browserAction.onClicked.addListener(setPluginStatus);
+
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	if (request.action == "setproxy") 
 	{
