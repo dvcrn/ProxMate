2806d41f7fa4db27071eda3dde25a8e7df0bf933
David Mohl <david.mohl@ymail.com>
2012-02-08 14:08:24 +0100

    Content Script änderung

--- firefox/data/sites/personalitycores.js
+++ firefox/data/sites/personalitycores.js
@@ -1,15 +1,7 @@
 $(document).ready(function() {
+	$("#header").append("<h2 class='subhl'>Thanks for installing! You are awesome! :)</h2>")
+	$("#header").append("<p><a id='prox-opts' href='"+getUrlFor("options/options.html")+"' target='_blank'>Click here to configure ProxMates!</a></p>");
 
-	var ressources = loadRessources();
-
-	$.when(ressources).done(function() {
-		var selfData = ressources.response.data;
-
-			$("#header").append("<h2 class='subhl'>Thanks for installing! You are awesome! :)</h2>")
-			$("#header").append("<p><a id='prox-opts' href='"+getUrlFor("options/options.html")+"' target='_blank'>Click here to configure ProxMates!</a></p>");
-
-			$(".subhl").css("margin-top", "-35px");
-			$(".subhl").css("font-size", "22px");
-	});
-
+	$(".subhl").css("margin-top", "-35px");
+	$(".subhl").css("font-size", "22px");
 });
