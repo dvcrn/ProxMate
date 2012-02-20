07b92a8416ace8acc57d61692a1d893d3cb8b5af
David Mohl <david.mohl@ymail.com>
2012-02-20 16:32:53 +0100

    Hulu proxy reset fix

--- chrome/sites/hulu.js
+++ chrome/sites/hulu.js
@@ -8,15 +8,23 @@ $.when(global, experimental).done(function() {
 	}
 
 	$(document).ready(function() {
-		var afterLoad = function() {
-			$(window).unload(resetProxy);
-			setTimeout(resetProxy, 5000);
+		if (getUrlParam("unblocked") != "true") {
+			$("#show-title-container").append(" --> <a id='prox-unblock' href='javascript:void(0);'>Unblock This</a> <--");
+			$("#prox-unblock").click(function() {
+				// Remove the hash from the url
+				var loc_tmp = window.location.href;
+				var index_tmp = loc_tmp.indexOf('#');
+				if (index_tmp > 0) {
+					proxifyUri(loc_tmp.substring(0, index_tmp) + "?unblocked=true");	
+				}
+			});
+		} else {
+			var afterLoad = (function() {
+				$(window).unload(resetProxy);
+				setTimeout(resetProxy, 5000);
+			})();
 		}
 
-		$("#show-title-container").append(" --> <a id='prox-unblock' href='javascript:void(0);'>Unblock This</a> <--");
-		$("#prox-unblock").click(function() {
-			proxifyUri(window.location.href, true);
-		});
 	});
 
 });
