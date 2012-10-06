dd4536d30c95b8c400cf737659f2a7069f04145c
David Mohl <david.mohl@ymail.com>
2012-10-06 10:22:49 +0200

    Static flattr button as requested by mozilla

--- firefox/data/proxy.js
+++ firefox/data/proxy.js
@@ -123,13 +123,6 @@ var loadOverlay = function (callback) {
 
 var loadBanner = function (callback) {
 	"use strict";
-	(function () {
-	    var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
-	    s.type = 'text/javascript';
-	    s.async = true;
-	    s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
-	    t.parentNode.insertBefore(s, t);
-	}());
 
 	// Load the overlay
 	$('<link>').attr('rel', 'stylesheet')
