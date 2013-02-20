afd035985f106773d98ffc7d3c57674000f2a021
David Mohl <david.mohl@ymail.com>
2013-02-20 09:59:12 +0100

    first banner draft, refs #10

--- chrome/modules/youtube.js
+++ chrome/modules/youtube.js
@@ -13,66 +13,83 @@ var youtube = checkStatus("status_general");
 var autounblock = checkStatus("status_youtube_autounblock");
 
 $.when(global, youtube, autounblock).done(function () {
-	"use strict";
-	if (!global.response.enabled || !youtube.response.enabled) {
-		return;
-	}
+    "use strict";
+    if (!global.response.enabled || !youtube.response.enabled) {
+        return;
+    }
 
-	$(document).ready(function () {
-		/*
-			This code is for reviewers. To understand what we are doing here and why.
+    $(document).ready(function () {
+        /*
+            This code is for reviewers. To understand what we are doing here and why.
 
-			ProxMate unblocks all links having &proxmate=active in their url.
-			Youtube loads the video async after the initial pagecall is done. The videostream checks if the sitecall comes from the same IP as the videocall. If it doesn't, the video will be blocked.
-			Means we have to proxy both.
+            ProxMate unblocks all links having &proxmate=active in their url.
+            Youtube loads the video async after the initial pagecall is done. The videostream checks if the sitecall comes from the same IP as the videocall. If it doesn't, the video will be blocked.
+            Means we have to proxy both.
 
-			The way we came up with:
-			- Get youtubes script for creating the video
-			- Replace the videourl inside that script and append &proxmate=active
-			- Execute the script again. This will replace the current video container with a new one, including our altered url.
+            The way we came up with:
+            - Get youtubes script for creating the video
+            - Replace the videourl inside that script and append &proxmate=active
+            - Execute the script again. This will replace the current video container with a new one, including our altered url.
 
-			Using a pac_script entry for this url doesn't work! Otherways we would unblock ALL youtube videos what we clearly don't want!
-		*/
-		var pmParam, script, scriptcontent, n;
-		pmParam = getUrlParam('proxmate');
+            Using a pac_script entry for this url doesn't work! Otherways we would unblock ALL youtube videos what we clearly don't want!
+        */
+        var proxmate_parameter, script, scriptcontent, n;
+        proxmate_parameter = getUrlParam('proxmate');
 
-		if (pmParam === "active") {
-			script = $("#watch-video script")[1]; // Get the second script tag inside the #watch-video element
-			if (script === undefined) {
-				script = $("#watch7-video script")[1]; // Get the second script tag inside the #watch-video element
-			}
-			scriptcontent = $(script).contents()[0].data; // Get the script content (a.k.a the function)
-			loadBanner(function () {
-				$("#page").css("margin-top", "0px");
-			});
-			// videoplayback%253F
-			n = scriptcontent.replace(/videoplayback%253F/g, "videoplayback%253Fproxmate%253Dactive%2526"); // Append our proxmate param so the pac script wil care of it
-			$("body").append($("<script />", {
-				html: n
-			}));
+        if (proxmate_parameter !== undefined) {
+            script = $("#watch-video script")[1]; // Get the second script tag inside the #watch-video element
+            if (script === undefined) {
+                script = $("#watch7-video script")[1]; // Get the second script tag inside the #watch-video element
+            }
+            scriptcontent = $(script).contents()[0].data; // Get the script content (a.k.a the function)
+            loadBanner(function () {
+                $("#page").css("margin-top", "0px");
+            });
 
-		} else {
-			if ($("#watch7-player-unavailable").length > 0) {
-				if (autounblock.response.enabled) {
-					// Change text
-					$(".content .message").html("ProxMate will unblock this video now :)");
-					$(".content .submessage").html("Just a moment.");
+            $.get(getUrlFor("elements/youtube-proxmatebar.html"), function (data) {
 
-					// Change Icon
-					$("#watch7-player-unavailable img").prop("src", getUrlFor("images/waitajax.gif"));
-					window.location.href = window.location.href + "&proxmate=active";
-				} else {
-					loadOverlay(function () {
-						// Change text
-						$(".content .message").html("ProxMate will unblock this video now :)");
-						$(".content .submessage").html("Just a moment.");
+                $('<link>').attr('rel', 'stylesheet')
+                    .attr('type', 'text/css')
+                    .attr('href', getUrlFor("elements/youtube-proxmatebar.css"))
+                    .appendTo('head');
 
-						// Change Icon
-						$("#watch7-player-unavailable img").prop("src", getUrlFor("images/waitajax.gif"));
-						window.location.href = window.location.href + "&proxmate=active";
-					});
-				}
-			}
-		}
-	});
+                $("#watch7-video-container").prepend(data);
+                $(".yt-proxmatebar p span").html(proxmate_parameter.toUpperCase());
+                $(".yt-proxmatebar .pm-logo").prop("src", getUrlFor("images/icon48.png"));
+                $(".yt-proxmatebar .pm-country-flag").prop("src", getUrlFor("images/" + proxmate_parameter + ".png"));
+
+            });
+
+
+            // videoplayback%253F
+            n = scriptcontent.replace(/videoplayback%253F/g, "videoplayback%253Fproxmate%253Dactive%2526"); // Append our proxmate param so the pac script wil care of it
+            $("body").append($("<script />", {
+                html: n
+            }));
+
+
+        } else {
+            if ($("#watch7-player-unavailable").length > 0) {
+                if (autounblock.response.enabled) {
+                    // Change text
+                    $(".content .message").html("ProxMate will unblock this video now :)");
+                    $(".content .submessage").html("Just a moment.");
+
+                    // Change Icon
+                    $("#watch7-player-unavailable img").prop("src", getUrlFor("images/waitajax.gif"));
+                    window.location.href = window.location.href + "&proxmate=active";
+                } else {
+                    loadOverlay(function () {
+                        // Change text
+                        $(".content .message").html("ProxMate will unblock this video now :)");
+                        $(".content .submessage").html("Just a moment.");
+
+                        // Change Icon
+                        $("#watch7-player-unavailable img").prop("src", getUrlFor("images/waitajax.gif"));
+                        window.location.href = window.location.href + "&proxmate=active";
+                    });
+                }
+            }
+        }
+    });
 });
