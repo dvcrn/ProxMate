c956170af0c4bdda38bad0fc87e1a6eaf0e61e17
David Mohl <david.mohl@ymail.com>
2013-09-10 21:48:18 +0900

    added correct bannerloading by evaluating the pac script

--- chrome-amd/ressources/scripts/page-worker/banner-loader.js
+++ chrome-amd/ressources/scripts/page-worker/banner-loader.js
@@ -1,12 +1,31 @@
-Proxmate.storage_get('offline_config', function (config) {
-    var config = JSON.parse(config);
-    var account_type = config.meta.account_type
+Proxmate.preferences_get('addon_is_active', function (global_status) {
+	if (global_status) {
+		Proxmate.storage_get(['offline_config', 'offline_pac_script'], function (stored_object) {
+			var config = stored_object.offline_config;
+			var pac = stored_object.offline_pac_script;
 
-    if (account_type === 'Free') {
-        $(document).ready(function () {
-            console.info("loading PM banner");
-            PmBanner.load_banner_stylesheet();
-            PmBanner.load_banner_html();
-        });
-    }
+			var shExpMatch = function (url, pattern) {
+				pattern = pattern.replace(/\./g, '\\.');
+				pattern = pattern.replace(/\*/g, '.*');
+				pattern = pattern.replace(/\?/g, '.');
+				var newRe = new RegExp('^' + pattern + '$');
+				return newRe.test(url);
+			};
+
+			eval(pac);
+
+			var config = JSON.parse(config);
+		    var account_type = config.meta.account_type
+		    var evaluated_pac = FindProxyForURL(window.location.href, window.location.hostname);
+
+
+		    if (account_type === 'Free' && evaluated_pac !== 'DIRECT') {
+		        $(document).ready(function () {
+		            console.info("loading PM banner");
+		            PmBanner.load_banner_stylesheet();
+		            PmBanner.load_banner_html();
+		        });
+		    }
+		});
+	}
 });
