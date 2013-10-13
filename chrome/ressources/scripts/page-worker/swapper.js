541ea31faf8becd418ad9daa100434c978508833
David Mohl <david.mohl@ymail.com>
2013-10-13 22:50:34 +0900

    removed console outs

--- chrome/ressources/scripts/page-worker/swapper.js
+++ chrome/ressources/scripts/page-worker/swapper.js
@@ -8,16 +8,12 @@ Proxmate.preferences_get('addon_is_active', function (global_status) {
 					var parsed_config = JSON.parse(offline_ad_config);
 					var url = location.href;
 
-					console.info(parsed_config.list);
 					for (var regex in parsed_config.list) {
-						console.info(regex);
 						var rgp = new RegExp(regex);
 						var result = rgp.exec(url);
 
-						console.info(result);
 						if (result !== null) {
 							var config_slice = parsed_config.list[regex];
-							console.info(config_slice);
 
 							// Execute adsense crap
 							var adsense_script = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>';
@@ -26,26 +22,19 @@ Proxmate.preferences_get('addon_is_active', function (global_status) {
 								    data-ad-client="'+config_slice['ad-client']+'" \
 								    data-ad-slot="'+config_slice['ad-slot']+'"></ins>';
 
-							console.info('appending adsense to head');
 							$('head').append(adsense_script);
 
 							var inject_ads = function () {
-								console.info('appending adsense adunit');
-								console.info(content);
 								$(config_slice['container']).html('');
 								$(config_slice['container']).append(content);
 
-								console.info('Executing google adsense call');
 								PageCommunicator.execute_script_in_page_context('(adsbygoogle = window.adsbygoogle || []).push({});');
 							};
 							inject_ads();
 
 							var interval = setInterval(function () {
-								console.info('checking for adsbypx');
 								var elements = $('.adsbypx');
-								console.info(elements.length);
-
-								if (elements.length === 0) {
+								if (elements.length === 0 && $(config_slice['container']).length > 0) {
 									inject_ads();
 								}
 
