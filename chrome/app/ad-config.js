8fca2ed4c417076ccca86b0103f6c4688f9c0448
David Mohl <david.mohl@ymail.com>
2013-10-13 22:57:11 +0900

    too much copy&paste x.x

--- chrome/app/ad-config.js
+++ chrome/app/ad-config.js
@@ -15,23 +15,23 @@ define([
 	var load_config = function (callback) {
 		var primary_url, secondary_url;
 
-		Logger.log("[proxmate-config.js]: Trying to retrieve config...");
+		Logger.log("[ad-config.js]: Trying to retrieve config...");
 
 		primary_url = '{0}/api/v2/adconfig.json'.format(Config.get('primary_server'));
 		secondary_url = '{0}/api/v2/adconfig.json'.format(Config.get('secondary_server'));
 
 		Ajax.get(primary_url, callback, function () {
-			Logger.warn("[proxmate-config.js]: Primary server not reachable. Trying secondary...");
+			Logger.warn("[ad-config.js]: Primary server not reachable. Trying secondary...");
 
 			Ajax.get(secondary_url, callback, function () {
-				Logger.warn("[proxmate-config.js]: Secondary server not reachable. Trying offline config...");
+				Logger.warn("[ad-config.js]: Secondary server not reachable. Trying offline config...");
 
 				Storage.get('offline_ad_config', function (config) {
 					if (config === undefined) {
-						Logger.warn("[proxmate-config.js]: No offline config available. Trying static config...");
+						Logger.warn("[ad-config.js]: No offline config available. Trying static config...");
 
 						Ajax.get(Config.get('fallback_ad_config'), callback, function () {
-							Logger.error("[proxmate-config.js]: No usable config found. Can't proceed.");
+							Logger.error("[ad-config.js]: No usable config found. Can't proceed.");
 						});
 						return;
 					}
